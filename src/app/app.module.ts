61973e89c05a5444905256db187f8db2cb641944
Ines Melero <imelero@vectoritcgroup.com>
2021-06-22 12:08:22 +0200

    Prueba de tecleo en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { PieComponent } from './pie/pie.component';
+import { TecleoModule } from './tecleo/tecleo.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PieComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PieComponent],
+  imports: [BrowserModule, AppRoutingModule, TecleoModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
