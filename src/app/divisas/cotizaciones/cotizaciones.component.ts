8bdd74a7715264f3afbdc0b5b70d547ef373027a
Ines Melero <imelero@vectoritcgroup.com>
2021-06-23 13:38:55 +0200

    Programando Divisas

--- src/app/divisas/cotizaciones/cotizaciones.component.ts
+++ src/app/divisas/cotizaciones/cotizaciones.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cotizaciones',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CotizacionesComponent implements OnInit {
 
+  @Input() public cotizaciones: any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
